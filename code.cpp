8460b5a536e274c82a9a4d8a17b976881c6f1413
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-26 21:11:26 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,12 +1,11 @@
 //Write a program in C++ to check the upper and lower limits of integer datatype.
-
 #include <iostream>
 using namespace std;
+
 int main () {
 	
 	cout << "The maxmimum value of int datatype is = " << INT_MAX << endl;
 	cout << "The minimum value of int datatype is = " << INT_MIN << endl;
 	
-
 	return 0;
 }
